bed6eab8f033cdf8fc870bc6b0d949951f570837
tkommareddy-rythmos <84847222+tkommareddy-rythmos@users.noreply.github.com>
2021-06-01 10:21:24 +0530

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sample2app.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.sample2app.model.Student;
-
-public interface StudentRepository extends MongoRepository<Student, Integer>{
-
-}
