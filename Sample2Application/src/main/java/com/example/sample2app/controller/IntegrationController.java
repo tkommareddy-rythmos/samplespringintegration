cb3d0c26f2c1fee78d75183956a0d2db60ea8224
tkommareddy-rythmos <84847222+tkommareddy-rythmos@users.noreply.github.com>
2021-06-04 11:25:45 +0530

    Add files via upload

--- Sample2Application/src/main/java/com/example/sample2app/controller/IntegrationController.java
+++ Sample2Application/src/main/java/com/example/sample2app/controller/IntegrationController.java
@@ -1,13 +1,16 @@
 package com.example.sample2app.controller;
 
+import java.util.UUID;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.sample2app.config.StudentConfig;
 import com.example.sample2app.model.Address;
 import com.example.sample2app.model.Student;
 import com.example.sample2app.service.IntegrationGateway;
@@ -17,15 +20,21 @@ import com.example.sample2app.service.IntegrationGateway;
 public class IntegrationController {
 	@Autowired
 	private IntegrationGateway integrationGateway;
-
+	 @Autowired
+	    private RabbitTemplate template;
+	
 	@PostMapping("/student")
 	public void processStudentDetails(@RequestBody Student student) {
 		integrationGateway.process(student);
 	}
 
-	@PostMapping("/address")
-	public void processAddressDetails(@RequestBody Address address) {
-		integrationGateway.process(address);
-	}
+	
+	 @PostMapping("/{details}")
+	    public String savedetails(@RequestBody Address address,@PathVariable String details) {
+		 address.setAddressid(UUID.randomUUID().toString());
+		 integrationGateway.process(address);
+		 template.convertAndSend(StudentConfig.EXCHANGE, StudentConfig.ROUTING_KEY, address);
+	        return "Success !!";
+	    }
 
 }
